11f07f5cb68693be81335fb5fc645d4d18a0bb61
Vinayaka S Yattinahalli <>
2024-01-25 22:18:33 +0530

    Initial commit

---
+++ Practice/Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Practice
+//
+//  Created by Vinayaka S Yattinahalli on 25/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
